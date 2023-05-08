216637a0a2f95bec7cb3cd0d54ee1d584d29885d
sulaimangari <sulaimangari97@gmail.com>
2023-05-08 22:42:14 +0700

    Update workflows and BSP (#6)
    - Update legacy work flow to Node.js 16 (Node.js 12 will be deprecated
    this month)
    - Update BSP and nvidia-repo to latest version (last jetpack for Jetson
    Nano)

--- create-image.sh
+++ create-image.sh
@@ -7,7 +7,7 @@
 
 set -e
 
-BSP=https://developer.nvidia.com/embedded/l4t/r32_release_v6.1/t210/jetson-210_linux_r32.6.1_aarch64.tbz2
+BSP=https://developer.nvidia.com/downloads/remetpack-463r32releasev73t210jetson-210linur3273aarch64tbz2
 
 # Check if the user is not root
 if [ "x$(whoami)" != "xroot" ]; then
